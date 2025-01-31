fa028cf092efa4d4298fcdb8241b68499e0b73d1
Dea Salsabilla <deasalsabilla19@gmail.com>
2025-01-31 15:40:41 +0700

    update tampilan kelola aplikasi

--- menu/kelola_app/app.php
+++ menu/kelola_app/app.php
@@ -142,44 +142,43 @@
                         <button class="mb-3 tf-btn accent small" style="width: 20%;">
                             <a href="tambah.php" style="color: white; text-decoration: none;" onmouseover="this.style.color='#533dea'" onmouseout="this.style.color='white'">Tambah Data</a>
                         </button>
-                        <table border="1" style="width: 100%; border-collapse: collapse; text-align: left;">
+                        <table class="table table-striped">
                             <thead>
-                                <tr style="background-color: #f2f2f2;">
-                                    <th style="padding: 10px;">No</th>
-                                    <th style="padding: 10px;">Nama Aplikasi</th>
-                                    <th style="padding: 10px;">Logo Aplikasi</th>
-                                    <th style="padding: 10px;">Tanggal Rilis</th>
-                                    <th style="padding: 10px;">Alamat</th>
-                                    <th style="padding: 10px;">No Telepon</th>
-                                    <th style="padding: 10px;">Aksi</th>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Aplikasi</th>
+                                    <th>Logo</th>
+                                    <th>Tanggal Rilis</th>
+                                    <th>Alamat</th>
+                                    <th>No Telepon</th>
+                                    <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
                                 include "../../conn/koneksi.php";
                                 $no = 1;
-
                                 $sql = mysqli_query($koneksi, "SELECT * FROM tb_app");
                                 while ($data = mysqli_fetch_array($sql)) {
-                                    echo "<tr>";
-                                    echo "<td style='padding: 10px;'>" . ($no++) . "</td>";
-                                    echo "<td style='padding: 10px;'>{$data['nm_app']}</td>";
-                                    echo "<td style='padding: 10px;'>{$data['logo_app']}</td>";
-                                    echo "<td style='padding: 10px;'>{$data['tgl_rilis']}</td>";
-                                    echo "<td style='padding: 10px;'>{$data['almt']}</td>";
-                                    echo "<td style='padding: 10px;'>{$data['no_telp']}</td>";
-                                    echo "<td style='padding: 10px;'>
-                                    <a href='edit.php?id={$data['id_app']}' style='margin-right: 10px;'>
-                                        <i class='fa fa-pencil'></i>
-                                    </a>
-                                    <a href='delete.php?id={$data['id_app']}' onclick=\"return confirm('Apakah Anda yakin ingin menghapus data ini?')\">
-                                        <i class='fa fa-trash'></i>
-                                    </a>
-                                </td>";
-                                    echo "</tr>";
-                                }
+                                    $tgl_rilis = date("d-m-Y", strtotime($data['tgl_rilis']));
                                 ?>
-
+                                    <tr>
+                                        <td><?php echo $no++; ?></td>
+                                        <td><?php echo $data['nm_app']; ?></td>
+                                        <td><img src="<?php echo $data['logo_app']; ?>" alt="Logo" width="50"></td>
+                                        <td><?php echo $tgl_rilis; ?></td>
+                                        <td><?php echo $data['almt']; ?></td>
+                                        <td><?php echo $data['no_telp']; ?></td>
+                                        <td>
+                                            <a href="edit.php?id=<?php echo $data['id_app']; ?>" class="btn btn-warning btn-sm">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                            <a href="delete.php?id=<?php echo $data['id_app']; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">
+                                                <i class="fas fa-trash-alt"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
                             </tbody>
                         </table>
                     </div>
